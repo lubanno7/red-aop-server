6a56659e7cde32ac15c444c6062e5dd9aab76859
lubanno7 <y.dragon.hu@aliyun.comom>
2018-07-03 16:19:27 +0800

    修改socket服务器异常处理

--- src/serverSocket.ts
+++ src/serverSocket.ts
@@ -17,10 +17,14 @@ export class SocketServer extends BaseServer {
 
     start?(): void {
         this.tcp = TCP.createServer((socket: TCP.Socket) => {
-            // socket.write(buffer) 
-            log('net', `接收到来自${socket.remoteAddress} : ${socket.remotePort}的连接`);
-            if (this.connected) {
-                this.connected(socket);
+            try {
+                // socket.write(buffer) 
+                log('net', `接收到来自${socket.remoteAddress} : ${socket.remotePort}的连接`);
+                if (this.connected) {
+                    this.connected(socket);
+                }
+            } catch (error) {
+                log('net', new Error(`建立连接时发生异常:${error}`));
             }
         });
         this.tcp.listen(this.port, this.hostName);
